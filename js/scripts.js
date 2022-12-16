613c5c6d43fa204a53824fa45e0043cd26fcde1d
“Francesca <dascanio.francesca@gmail.com>
2022-12-16 16:22:39 +0100

    Completato esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
 //Chiedo nome all'utente
 const nome = prompt ('Digita il tuo nome di seguito');
-console.log("Il nome dell'utente è: ", nome);
-
-let 
+console.log("Il nome dell'utente è: ", nome.toLowerCase());
 
 //Chiedo cognome all'utente
 const cognome = prompt ('Digita il tuo cognome di seguito');
-console.log("Il cognome dell'utente è: ", cognome);
+console.log("Il cognome dell'utente è: ", cognome.toLowerCase());
 
 //Chiedo colore preferito dell'utente
 const colore = prompt ('Digita il tuo colore preferito di seguito');
-console.log("Il colore preferito dell'utente è: ", colore);
+console.log("Il colore preferito dell'utente è: ", colore.toLowerCase());
 
 //Creazione password
-let psw = nome + cognome + colore + 21;
-console.log('La password digitata è: ', nome + cognome + colore + 21);
+let psw = nome.toLowerCase() + cognome.toLowerCase() + colore.toLowerCase() + 21;
+console.log('La password digitata è: ', nome.toLowerCase() + cognome.toLowerCase() + colore.toLowerCase() + 21);
 
 //Visualizzazione password in HTML
 document.getElementById('my-password').innerHTML = psw; 
